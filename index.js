ae29d6b3fc77640a59ab3623acc9593952723aa7
mohammed rayaan pasha <mdrayaanpasha@gmail.com>
2026-01-05 19:36:52 +0530

    testing if CI CD WEOKING?

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ status: "ok" });
+  res.json({ status: "its ok" });
 });
 
 app.listen(PORT, () => {

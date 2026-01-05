cd9cfd741211e7ed95f4d4682e922f152f860490
mohammed rayaan pasha <mdrayaanpasha@gmail.com>
2026-01-05 19:41:20 +0530

    new

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ status: "its ok" });
+  res.json({ status: "it ok" });
 });
 
 app.listen(PORT, () => {
